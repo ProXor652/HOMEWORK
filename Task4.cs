3184fa08ae4790163fc0766ec8ee0b678bc42f05
ProXor652 <apmetalnikov@gmail.com>
2022-09-04 12:56:27 +0700

    Task 1

--- Task4.cs
+++ Task4.cs
@@ -1,1 +1,21 @@
-﻿
+﻿/* //Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+ Console.WriteLine("Enter first number: " );
+ int a = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Enter two number: ");
+ int b = int.Parse(Console.ReadLine());
+
+ int fun(int a)
+ {
+    int sum = 0;
+    for (int i = a; i <= b; ++i)
+    {
+        sum+= i;
+    }
+    return sum;
+ }
+
+ Console.WriteLine(fun(a)); */
+
+ 
